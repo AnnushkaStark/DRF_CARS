31f87e9aa465444e721c9f24e694878ace6d2043
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2024-04-22 00:16:36 +0300

    добавление экземпляров модели

--- car_project/main_app/urls.py
+++ car_project/main_app/urls.py
@@ -1,3 +1,12 @@
+from django.urls import path
+
+from .views import *
+
 app_name = "main_app"
 
-urlpatterns = []
+urlpatterns = [
+    path("api/add_country/", AddCountryApiView.as_view(), name="add_country"),
+    path("api/add_manufacturer/", AddManufacturerApiView.as_view(), name="add_manufacturer"),
+    path("api/car/", AddCarApiView.as_view(), name="add_car"),
+    path("api/comment/", AddCommentApiView.as_view(), name="add_comment")
+]
--- car_project/main_app/views.py
+++ car_project/main_app/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView, status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import(
+    AddCarSerializer,
+    AddCommentSerializer,
+    AddCountrySerializer,
+    AddManufacturerSerializer
+)
+
+
+class AddCountryApiView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        country = AddCountrySerializer(data=request.data)
+        country.is_valid(raise_exception=True)
+        country.save()
+        return Response(
+            {"success": "Старна добавлена."},
+            status=status.HTTP_201_CREATED,
+    )
+
+
+class AddManufacturerApiView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        manufacturer = AddManufacturerSerializer(data=request.data)
+        manufacturer.is_valid(raise_exception=True)
+        manufacturer.save()
+        return Response(
+                {"success": "Производитель добавлен."},
+                status=status.HTTP_201_CREATED,
+    )
+    
+
+class AddCarApiView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        car = AddCarSerializer(data=request.data)
+        car.is_valid(raise_exception=True)
+        car.save()
+        return Response(
+                {"success": "Автомобиль добавлен."},
+                status=status.HTTP_201_CREATED,
+    )
+
+
+class AddCommentApiView(APIView):
+    def post(self,request, *args, **kwargs):
+        comment = AddCommentSerializer(data=request.data)
+        comment.is_valid(raise_exception=True)
+        comment.save()
+        return Response(
+                {"success": "Комментарий добавлен."},
+                status=status.HTTP_201_CREATED,
+    )
+
+    
